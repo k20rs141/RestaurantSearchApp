6eeb77528da78a13dac8c25cb5149bd6718e441e
YAMADA KOUKI <k20rs141@st.kyusan-u.ac.jp>
2023-05-29 23:37:51 +0900

    Improve: APIや位置情報エラー時のアラート表示と詳細画面への画面遷移

--- RestaurantSearchApp/RestaurantSearchApp/Model/LocationManager.swift
+++ RestaurantSearchApp/RestaurantSearchApp/Model/LocationManager.swift
@@ -4,19 +4,18 @@ class LocationManager: NSObject, CLLocationManagerDelegate {
     static let shared = LocationManager()
     let locationManager = CLLocationManager()
     var userLocation: CLLocation?
-    var denied = false
     
     override init() {
         super.init()
 
         locationManager.delegate = self
         locationManager.desiredAccuracy = kCLLocationAccuracyHundredMeters
-        locationManager.startUpdatingLocation()
     }
     
     func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
         guard let location = locations.last else { return }
         userLocation = location
+        print("userLocation: \(userLocation)")
     }
     
     func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
@@ -34,7 +33,6 @@ class LocationManager: NSObject, CLLocationManagerDelegate {
         // ユーザーが設定から位置情報を許可してない場合
         case .denied:
             manager.requestWhenInUseAuthorization()
-            denied = true
         // ユーザーが位置情報を許可している場合
         case .authorizedAlways, .authorizedWhenInUse:
             manager.requestLocation()
--- RestaurantSearchApp/RestaurantSearchApp/View/ViewController.swift
+++ RestaurantSearchApp/RestaurantSearchApp/View/ViewController.swift
@@ -4,21 +4,6 @@ import MapKit
 class ViewController: UIViewController {
     let locationManager = LocationManager.shared
     var searchWord: String = ""
-    var range = 3
-    var error: [Errors]? {
-        didSet {
-            if let error = error?.first {
-                switch error.code {
-                case 1000:
-                    present(.showAPIErrorAlert(title: "サーバ障害エラー", message: error.message))
-                case 2000:
-                    present(.showAPIErrorAlert(title: "APIキーまたはIPアドレスの認証エラー", message: error.message))
-                default:
-                    present(.showAPIErrorAlert(title: "パラメータ不正エラー", message: error.message))
-                }
-            }
-        }
-    }
     var shops = [Shop]() {
         didSet {
             tableView.reloadData()
@@ -42,54 +27,32 @@ class ViewController: UIViewController {
         tableView.dataSource = self
         tableView.delegate = self
         searchText.delegate = self
-        checkAuthorizationStatus()
-        fetchGourmet()
     }
-
-    func fetchGourmet() {
-        var gourmetSearchURL = ""
+    
+    private func fetchGourmet() {
         // 予約文字をエンコード
         guard let encodeSearchWord = searchWord.addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed) else { return }
-        if let userLocation = locationManager.userLocation?.coordinate {
-            gourmetSearchURL = Constants.baseURL + apiKey + "&lat=\(userLocation.latitude)&lng=\(userLocation.longitude)&keyword=\(encodeSearchWord)&range=\(range)&count=50&format=json"
-        } else {
-            gourmetSearchURL = Constants.baseURL + apiKey + "&lat=35.68944&lng=139.69167&keyword=\(encodeSearchWord)&range=\(range)&count=50&format=json"
-        }
+        guard let userLocation = locationManager.userLocation?.coordinate else { return }
 
+        let gourmetSearchURL = Constants.baseURL + apiKey + "&lat=\(userLocation.latitude)&lng=\(userLocation.longitude)&keyword=\(encodeSearchWord)&count=30&format=json"
+        
         Task {
             do {
                 let response = try await HotPepperAPIService.shared.request(with: gourmetSearchURL)
-                if let shops = response.results.shop {
-                    if shops.count == 0 {
-
-                    } else {
-                        DispatchQueue.main.async {
-                            self.shops = shops
-                        }
+                DispatchQueue.main.async {
+                    if let shops = response.results.shop {
+                        self.shops = shops
                     }
-                } else if let error = response.results.error {
-                    self.error = error
                 }
             } catch {
                 print("unknown: \(error.localizedDescription)")
             }
         }
     }
-
-    private func checkAuthorizationStatus() {
-        if locationManager.denied {
-            print("test: \(locationManager.denied)")
-            present(.showLocationAlert(title: "位置情報をオンにして下さい", message: "位置情報を利用して店舗検索を行います。設定から位置情報の許可をお願いします。"))
-        }
-    }
 }
 
 extension ViewController: UITableViewDataSource {
     public func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 1
-    }
-    
-    public func numberOfSections(in tableView: UITableView) -> Int {
         return shops.count
     }
 
@@ -97,20 +60,14 @@ extension ViewController: UITableViewDataSource {
         guard let cell = tableView.dequeueReusableCell(withIdentifier: "RestaurantCustomCell", for: indexPath) as? RestaurantCustomCell else {
             return UITableViewCell()
         }
-        cell.shop = shops[indexPath.section]
-
+        cell.shop = shops[indexPath.row]
         return cell
     }
 }
 
 extension ViewController: UITableViewDelegate {
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        tableView.deselectRow(at: indexPath, animated: true)
 
-        let storyboard: UIStoryboard = UIStoryboard(name: "ShopDetail", bundle: nil)
-        guard let shopDetailViewController = storyboard.instantiateViewController(withIdentifier: "ShopDetailViewController") as? ShopDetailViewController else { return }
-        shopDetailViewController.shopDetail = shops[indexPath.section]
-        self.navigationController?.pushViewController(shopDetailViewController, animated: true)
     }
 }
 
@@ -118,7 +75,7 @@ extension ViewController: UISearchBarDelegate {
     func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
         searchBar.endEditing(true)
         guard let searchWord = searchBar.text, !searchWord.isEmpty else {
-            return present(.emptySearchTextAlert(title: "検索欄が空白です！", message: "キーワードを入力してください。"))
+            return UIAlertController.emptySearchTextAlert(self)
         }
         self.searchWord = searchWord
         fetchGourmet()
